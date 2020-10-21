8733ca6ef5505491c606b9dd357dc3b74a4a3b59
drf <dany.francisco@outlook.com>
2020-10-21 22:59:54 +0200

    chore(openDataParis): init all our project

---
+++ src/index.js
@@ -0,0 +1,9 @@
+// Dependencies
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './index.scss';
+
+const App = () => <div>Hello world</div>;
+
+ReactDOM.render(<App />, document.getElementById('app'));
